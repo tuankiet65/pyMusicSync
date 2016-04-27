7115897516ec0aff06a0ea4458a4c72ae937b97e
Ho Tuan Kiet <tuankiet65@gmail.com>
2016-04-27 14:01:41 +0700

    Breaks objects.py into multiple files

---
+++ pyMusicSync/objects/progress.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import threading
+
+
+class Progress:
+    threadLock = threading.Lock()
+    finished = 0
+    total = 0
+    percent = 0.0
+
+    def __init__(self):
+        pass
+
+    def increase(self):
+        with self.threadLock:
+            self.finished += 1
+            self.percent = (self.finished / self.total) * 100
